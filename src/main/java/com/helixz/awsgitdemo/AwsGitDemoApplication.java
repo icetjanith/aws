33e9e082597fe0004527e06a4b159abfcccb7c24
chamith kodikara <chamith@helixzsolutions.com>
2025-02-17 00:29:53 +0100

    -initial commit

---
+++ src/main/java/com/helixz/awsgitdemo/AwsGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.helixz.awsgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsGitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/helixz/awsgitdemo/AwsGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helixz.awsgitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsGitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
